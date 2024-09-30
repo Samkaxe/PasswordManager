b1d1fa1efdd2303c3c7a260e68926b7adced4351
Renaldinho <rena0968@easv365.dk>
2024-09-30 20:12:48 +0200

    wow made a page

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,23 +1,28 @@
-import React, {useState} from 'react';
-import logo from './logo.svg';
+import React, {StrictMode, useState} from 'react';
 import './App.css';
 import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 import ManageCredentialsPage from "./components/ManageCredentialsPage/ManageCredentialsPage";
 import SignInPage from "./components/SignInPage/SignInPage";
+import {ChakraProvider, ColorModeScript} from "@chakra-ui/react";
 
 function App() {
 
     const [isAuthenticated, setIsAuthenticated] = useState(false); // Initially not authenticated
 
-
     return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="*" element={isAuthenticated ? <Navigate to="/manage-credentials" replace /> : <Navigate to="/sign-in" replace />} />
-          <Route path="/sign-in" element={<SignInPage />} />
-          <Route path="/manage-credentials" element={<ManageCredentialsPage />} />
-        </Routes>
-      </BrowserRouter>
+    <StrictMode>
+        <ColorModeScript initialColorMode='dark' />
+        <ChakraProvider>
+
+            <BrowserRouter>
+                <Routes>
+                    <Route path="*" element={isAuthenticated ? <Navigate to="/manage-credentials" replace /> : <Navigate to="/sign-in" replace />} />
+                    <Route path="/sign-in" element={<SignInPage />} />
+                    <Route path="/manage-credentials" element={<ManageCredentialsPage />} />
+                </Routes>
+            </BrowserRouter>
+        </ChakraProvider>
+    </StrictMode>
   );
 }
 
--- ui/src/components/SignInPage/SignInPage.tsx
+++ ui/src/components/SignInPage/SignInPage.tsx
@@ -1,12 +1,52 @@
-import React, { FC } from 'react';
-
+import React, { FC, useState } from 'react';
+import {
+    Button,
+    FormControl,
+    FormLabel,
+    Input,
+    InputGroup,
+    InputRightElement,
+    VStack,
+    IconButton,
+} from '@chakra-ui/react';
+import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
 
 interface SignInPageProps {}
 
-const SignInPage: FC<SignInPageProps> = () => (
-  <div>
-    SignInPage Component
-  </div>
-);
+const SignInPage: FC<SignInPageProps> = () => {
+    const [showPassword, setShowPassword] = useState(false);
+
+    const handleTogglePasswordVisibility = () => setShowPassword(!showPassword);
+
+    return (
+        <div className="w-full h-full justify-center items-center flex pb-40">
+            <VStack spacing={4} align="stretch" p={8} maxW="md">
+                <FormControl>
+                    <FormLabel>Email</FormLabel>
+                    <Input type="email" placeholder="Enter your email" />
+                </FormControl>
+
+                <FormControl>
+                    <FormLabel>Password</FormLabel>
+                    <InputGroup>
+                        <Input type={showPassword ? 'text' : 'password'} placeholder="Enter your password" />
+                        <InputRightElement width="4.5rem">
+                            <IconButton
+                                aria-label={showPassword ? 'Hide password' : 'Show password'}
+                                icon={showPassword ? <ViewOffIcon /> : <ViewIcon />}
+                                onClick={handleTogglePasswordVisibility}
+                            />
+                        </InputRightElement>
+                    </InputGroup>
+                </FormControl>
+
+                <Button colorScheme="blue" width="full">
+                    Sign In
+                </Button>
+            </VStack>
+
+        </div>
+    );
+};
 
 export default SignInPage;
