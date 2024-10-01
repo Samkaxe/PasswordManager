70ec247aa612e4f583722293e218901228dd052e
Renaldinho <rena0968@easv365.dk>
2024-10-01 15:07:35 +0200

    .

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -4,21 +4,21 @@ import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 import ManageCredentialsPage from "./components/ManageCredentialsPage/ManageCredentialsPage";
 import SignInPage from "./components/SignInPage/SignInPage";
 import {ChakraProvider, ColorModeScript} from "@chakra-ui/react";
+import ResourceNotFoundPage from "./components/ResourceNotFoundPage/ResourceNotFoundPage";
 
 function App() {
 
-    const [isAuthenticated, setIsAuthenticated] = useState(false); // Initially not authenticated
+    const [isAuthenticated, setIsAuthenticated] = useState(true); // Initially not authenticated
 
     return (
     <StrictMode>
-        <ColorModeScript initialColorMode='dark' />
         <ChakraProvider>
-
             <BrowserRouter>
                 <Routes>
                     <Route path="*" element={isAuthenticated ? <Navigate to="/manage-credentials" replace /> : <Navigate to="/sign-in" replace />} />
                     <Route path="/sign-in" element={<SignInPage />} />
                     <Route path="/manage-credentials" element={<ManageCredentialsPage />} />
+                    <Route path="/not-found" element={<ResourceNotFoundPage /> } />
                 </Routes>
             </BrowserRouter>
         </ChakraProvider>
---
+++ ui/src/components/ResourceNotFoundPage/ResourceNotFoundPage.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+
+
+interface ResourceNotFoundPageProps {}
+
+const ResourceNotFoundPage: FC<ResourceNotFoundPageProps> = () => (
+  <div>
+    ResourceNotFoundPage Component
+  </div>
+);
+
+export default ResourceNotFoundPage;
