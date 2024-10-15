d5e598a343bb57d70933772b695760ef995119e2
Renaldinho <rena0968@easv365.dk>
2024-10-15 19:18:13 +0200

    azure vault

--- API/Program.cs
+++ API/Program.cs
@@ -4,6 +4,7 @@ using API.Dal.IRepo;
 using API.Dal.Repo;
 using API.Interfaces;
 using API.Services;
+using Azure.Identity;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
@@ -16,13 +17,20 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var keyVaultUrl = new Uri(builder.Configuration.GetSection("AzureVault:Url").Value!);
+var azureCredential = new ClientSecretCredential(
+    builder.Configuration.GetSection("AzureVault:AzureClientTenantId").Value!,
+    builder.Configuration.GetSection("AzureVault:AzureClientId").Value!,
+    builder.Configuration.GetSection("AzureVault:AzureClientSecret").Value!);
+
+builder.Configuration.AddAzureKeyVault(keyVaultUrl, azureCredential);
 
 // Register DbContext and repositories
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddScoped<EncryptionHelper>(provider => 
-        new EncryptionHelper("mySuperSecretKey1234567890") // Or retrieve the key from configuration
+        new EncryptionHelper(builder.Configuration.GetSection("EncryptionHelper").Value) // Or retrieve the key from configuration
 );
 builder.Services.AddScoped<ITokenService, TokenService>();
 builder.Services.AddScoped<IUserService, UserService>();
@@ -40,9 +48,9 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             ValidateLifetime
                 = true,
             ValidateIssuerSigningKey = true,
-            ValidIssuer = builder.Configuration["Jwt:Issuer"],
-            ValidAudience = builder.Configuration["Jwt:Audience"],
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))   
+            ValidIssuer = builder.Configuration[builder.Configuration.GetSection("JwtIssuer").Value],
+            ValidAudience = builder.Configuration[builder.Configuration.GetSection("JwtAudience").Value],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration[builder.Configuration.GetSection("JwtKey").Value])) 
 
         };
     });
